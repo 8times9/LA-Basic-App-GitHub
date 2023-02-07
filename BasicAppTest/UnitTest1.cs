0c7576ca2f89fa869ae84777054011c09038a7b0
Luke Arnould <luke.arnould@okstate.edu>
2023-02-07 13:09:47 -0600

    Add project files.

---
+++ BasicAppTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using LA_Basic_App_GitHub;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BasicAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 tp = new Class1();
+            Assert.AreEqual(tp.Name, "[Luke Arnould] - The Code Master");
+        }
+    }
+}
